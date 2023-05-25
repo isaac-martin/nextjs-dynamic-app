3dd1f3ad9a254db91615a78b272eee399a31fcf4
Isaac Martin <isaac@brex.com>
2023-05-25 17:45:40 -0400

    update comment

--- middleware.ts
+++ middleware.ts
@@ -22,7 +22,7 @@ export async function middleware(request: NextRequest) {
     return page;
   }
 
-  // This fails with error about not setPageProp
+  // This fails with error about not res.setPageProp is not a function (which looking on the return types is correct, however it does contradict what the docs say)
   // based on the docs https://docs.netlify.com/integrations/frameworks/next-js/middleware/?_gl=1%2ahuuwhy%2a_gcl_au%2aNDUwNjY0MjU3LjE2ODQxNjgzNzU.&_ga=2.145646900.1046593485.1685028022-945217984.1668549303#middlewareresponse-object
   // this res should be a MiddlewareResponse and behave the same as .next()
   if (request.nextUrl.pathname.startsWith('/both')) {
@@ -37,7 +37,7 @@ export async function middleware(request: NextRequest) {
 
   // This 404s because the rewrite is not returned
   if (request.nextUrl.pathname.startsWith('/alternative-both')) {
-    const res = await netlifyMiddlewareRequest.rewrite(new URL('/', request.url));
+    await netlifyMiddlewareRequest.rewrite(new URL('/', request.url));
     // trying to use next before setting props
     const finalRes = await netlifyMiddlewareRequest.next();
 
